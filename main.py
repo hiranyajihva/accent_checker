de25a7f94d9caf3d5a0a7a483bdf8dad77d926f8
Hiranyajihva <88590577+hiranyajihva@users.noreply.github.com>
2023-04-22 14:04:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+def accent_checker():
+
+    # First allowing user input
+
+    print("Hello, please input the word which you would like to check if it is accented or not.")
+    user_input_word = input("Please enter the word which you would like to check whether it is accented or not: ")
+
+    if 'à' or 'è' 'ì' 'ò' or 'ù' or 'À' or 'È' or 'Ì' or 'Ò' or 'Ù' in user_input_word:
+        print('\n')
+        print("The word contains an acute accent ")
+    elif 'â' or 'ê' or 'î' or 'ô' or 'û' or 'Â' or 'Ê' or 'Î' or 'Ô' or 'Û' in user_input_word:
+        print('\n')
+
+    else:
+        print("The word is unaccented.")
+
+
+accent_checker()
